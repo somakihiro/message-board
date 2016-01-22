26f0f44f56a081f613d006b51819a15c6ef11568
water23 <aki.air@icloud.com>
2016-01-22 14:24:12 +0000

    add of aeg column

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates :name , length: { maximum: 20 } , presence: true
     # 内容は必須入力かつ2文字以上30文字以内
     validates :body , length: { minimum: 2, maximum: 30 }, presence: true
+    # 年齢は0以上の数字
+    validates :age , length: { minimum: 0 }
 end
